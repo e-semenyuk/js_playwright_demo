452770cc7896e917b9a054217a7100ea247879f8
Eugene Semenyuk <e.semenyuk@outlook.com>
2025-04-26 13:45:28 +0300

    Create LoginPage for KAN-88

---
+++ pages/LoginPage.js
@@ -0,0 +1,18 @@
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = page.locator('[data-test="username"]');
+    this.passwordInput = page.locator('[data-test="password"]');
+    this.loginButton = page.locator('[data-test="login-button"]');
+    this.userMenu = page.locator('[data-test="user-menu"]');
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+    await expect(this.userMenu).toBeVisible(); // Verify login success
+  }
+}
+
+module.exports = LoginPage;
